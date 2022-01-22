a7ebb1b93f926aff2202d2fe60ad57ee0da3abf0
roneig <grimmronei@gmail.com>
2022-01-22 15:52:00 -0300

    Desenvolvimento dos layouts de listagem e detalhe de partidas..

---
+++ app/src/main/java/me/dio/simulator/MainActivityKotlin.kt
@@ -0,0 +1,19 @@
+package me.dio.simulator
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import me.dio.simulator.databinding.ActivityMainBinding
+
+class MainActivityKotlin : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+        //val tvHello = findViewById<TextView>(R.id.tvHello)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //binding.tvHello.text = "Teste"
+    }
+}
